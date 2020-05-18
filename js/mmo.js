0f6fff038a7111762a362dedb5389b7fcdf1be41
Louis Machin <ldhmachin@gmail.com>
2020-05-18 16:24:40 +0100

    Added MMO basic code

---
+++ js/mmo.js
@@ -0,0 +1,73 @@
+let canvas;
+let player;
+let ghosts = [];
+let database;
+
+function setup() {
+	// Firebase
+	var firebaseConfig = {
+		apiKey: "AIzaSyAmxjDLZrtiWjQGkekCdTXKx5zCbLDJP28",
+		authDomain: "machin-dev.firebaseapp.com",
+		databaseURL: "https://machin-dev.firebaseio.com",
+		projectId: "machin-dev",
+		storageBucket: "machin-dev.appspot.com",
+		messagingSenderId: "471389656934",
+		appId: "1:471389656934:web:de807290048ad37fa91aa7",
+		measurementId: "G-2LDMEXJL3N"
+	};
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
+	firebase.analytics();
+	database = firebase.database();
+	var ref = database.ref('mmo/players');
+  	ref.on('value', gotData, errData);
+	// Canvas
+	canvas = createCanvas(640, 360);
+	canvas.parent('wrapper');
+	canvas.mouseClicked(move);
+	noLoop();
+	// Player
+	var name = getURLParams().name;
+	if (name == null) {
+		name = "Null";
+	}
+	player = new Player(name, random(width), random(height), createVector(0, 0));
+}
+
+function getName() {
+	return name;
+}
+
+function draw() {
+	background(100);
+	// Display player
+	player.display();
+	// Display ghosts
+	for (var i = 0; i < ghosts.length; i++) {
+		ghosts[i].display();
+	};
+}
+
+function move() {
+	player.move(mouseX, mouseY);
+}
+
+function gotData(data) {
+	ghosts = [];
+	var records = data.val();
+	var names = Object.keys(records);
+	for (var i = 0; i < names.length; i++) {
+		var name = names[i];
+		if (name != player.getName) {
+			var x = records[name].x;
+			var y = records[name].y;
+			var dir = createVector(records[name].dir[0], records[name].dir[1]);
+			ghosts.push(new Player(name, x, y, dir));
+		}
+	};
+	draw();
+}
+
+function errData(err) {
+
+}
---
+++ js/player.js
@@ -0,0 +1,36 @@
+class Player {
+  constructor(name, x, y, dir) {
+  	this.name = name
+    this.x = x;
+    this.y = y;
+    this.dir = dir;
+  }
+
+  move(x, y) {
+  	this.dir = createVector(x - this.x, y - this.y).setMag(32);
+    this.x = x;
+    this.y = y;
+    this.submit();
+  }
+
+  display() {
+  	fill(255);
+    ellipse(this.x, this.y, 16, 16);
+    line(this.x, this.y, this.x + this.dir.x, this.y + this.dir.y);
+  	text(this.name, this.x, this.y + 32);
+  }
+
+  submit() {
+  	var data = {
+		x: this.x,
+		y: this.y,
+		dir: [this.dir.x, this.dir.y]
+  	};
+  	var ref = database.ref('mmo/players');
+  	ref.child(this.name).set(data, this.gotData);
+  }
+
+  gotData(data) {
+
+  }
+}
