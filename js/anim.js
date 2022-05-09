ed64681815b3e3c3d35bb1e2a34e4a69d8352817
Louis Machin <ldhmachin@gmail.com>
2022-05-09 09:41:59 +0100

    Added milkbox now playing

--- js/anim.js
+++ js/anim.js
@@ -1,38 +1,42 @@
-var pre;
-var quote;
-var quotes;
-var split_string;
-var partition = 0;
-const str_width = 82;
-
-function preload() {
-    quotes = loadStrings('../motd.txt');
+var anim_sketch = function(p) {
+    p.pre;
+    p.quote;
+    p.quotes;
+    p.split_string;
+    p.partition = 0;
+    p.str_width = 82;
+    
+    p.preload = function() {
+        p.quotes = p.loadStrings('../motd.txt');
+    }
+    
+    p.setup = function() {
+        p.noCanvas();
+        p.frameRate(4);
+        // Assign DOM element
+        p.pre = p.select('#anim');
+        // Get quote phrase and pad
+        p.quote = " " + p.random(p.quotes) + " ";
+        p.partition = p.str_width;
+        p.quote += ("·").repeat(p.str_width - p.quote.length);
+    }
+    
+    p.draw = function() {
+        // Calculate string split by partition and rejoined
+        p.split_string = 
+            p.quote.substr(p.str_width - p.partition, p.str_width) + 
+            p.quote.substr(0, p.str_width - p.partition);
+        // Construct ASCII output // ═=║ |-
+        var inner = "";
+        inner += "┌" + ("─").repeat(p.str_width) + "┐\n";
+        inner += "│" + p.split_string + "│\n";
+        inner += "└" + ("─").repeat(p.str_width) + "┘";
+        // Assign inner HTML to DOM element
+        p.pre.html(inner);
+        // Iterate partition index
+        //partition = (partition + 1) % str_width;
+        p.partition = p.partition == 0 ? p.str_width : p.partition - 1
+    }
 }
 
-function setup() {
-    noCanvas();
-    frameRate(4);
-    // Assign DOM element
-    pre = select('#anim');
-    // Get quote phrase and pad
-    quote = " " + random(quotes) + " ";
-    partition = str_width;
-    quote += ("·").repeat(str_width - quote.length);
-}
-
-function draw() {
-    // Calculate string split by partition and rejoined
-    split_string = 
-        quote.substr(str_width - partition, str_width) + 
-        quote.substr(0, str_width - partition);
-    // Construct ASCII output // ═=║ |-
-    inner = "";
-    inner += "┌" + ("─").repeat(str_width) + "┐\n";
-    inner += "│" + split_string + "│\n";
-    inner += "└" + ("─").repeat(str_width) + "┘";
-    // Assign inner HTML to DOM element
-    pre.html(inner);
-    // Iterate partition index
-    //partition = (partition + 1) % str_width;
-    partition = partition == 0 ? str_width : partition - 1
-}
+var anim_p5 = new p5(anim_sketch);
---
+++ js/milkbox.js
@@ -0,0 +1,49 @@
+var milkbox_sketch = function(p) {
+    p.pre;
+    p.playing;
+    p.split_string;
+    p.partition = 0;
+    p.str_width = 56;
+    
+    p.preload = function() {
+        p.playing = p.loadJSON('./api/metadata.json');
+        console.log(p.playing);
+    }
+
+    p.setup = function() {
+        p.noCanvas();
+        p.frameRate(4);
+        // Assign DOM element
+        p.pre = p.select('#milkbox');
+        // Get quote phrase and pad
+        var str = p.playing.artist + ' - ' + p.playing.track + ' (' + p.playing.collection + ')';
+        str = str.substr(0, p.str_width - 5);
+        p.quote = " " + str + " ";
+        p.partition = p.str_width;
+        p.quote += ("·").repeat(p.str_width - p.quote.length - 1);
+    }
+    
+    p.draw = function() {
+        // Calculate string split by partition and rejoined
+        p.split_string = 
+            p.quote.substr(p.str_width - p.partition, p.str_width) + 
+            p.quote.substr(0, p.str_width - p.partition);
+        // Construct ASCII output // ═=║ |-
+        var inner = "";
+        inner += "▗" + ("╍").repeat(p.str_width) + "▖  │   █   █ ▄▄█▄▄ █   █ ▄ │\n";
+        inner += "┇ " + p.split_string + "┇  │       ▄▄▄▄  ▄▄▄▄▄ ▄   ▄\n";
+        inner += "▝" + ("╍").repeat(p.str_width) + "▘  │       █   █ █     █   █";
+        // Assign inner HTML to DOM element
+        p.pre.html(inner);
+        // Iterate partition index
+        //partition = (partition + 1) % str_width;
+        p.partition = p.partition == 0 ? p.str_width : p.partition - 1
+    }
+}
+
+var milkbox_p5 = new p5(milkbox_sketch);
+
+
+// │       ▄▄▄▄  ▄▄▄▄▄ ▄   ▄
+// │       █   █ █     █   █
+// 
